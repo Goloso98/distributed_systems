2da7f59de17aa0d60c8448edf49fad2b78f0ea14
Gabriel <gooloso98@gmail.com>
2022-12-20 10:31:17 +0000

    gossiping

--- src/gossiping/main/gossiping.py
+++ src/gossiping/main/gossiping.py
@@ -1,14 +1,17 @@
 from concurrent import futures
-from numpy.random import choice, poisson, random
+from numpy.random import choice, poisson, random, randint
+from time import sleep
+import json
 import logging
+import sys
 import threading
 
 from gossiping_pb2 import Data, Reg, Void
-import gossiping_pb2_grpc
+import gossiping_pb2_grpc as rpc
 import grpc
 
 
-class Gossiping(gossiping_pb2_grpc.GossipingServicer):
+class Gossiping(rpc.GossipingServicer):
 
     def __init__(self, shared):
         self.shared = shared
@@ -20,7 +23,7 @@ class Gossiping(gossiping_pb2_grpc.GossipingServicer):
 
     def LocalRegister(self, request, context):
         with grpc.insecure_channel(request.ip) as channel:
-            stub = gossiping_pb2_grpc.GossipingStub(channel)
+            stub = rpc.GossipingStub(channel)
             response = stub.Register(Reg(ip=self.uri))
         self.shared.addPeer(request.ip)
         return Void()
@@ -28,7 +31,10 @@ class Gossiping(gossiping_pb2_grpc.GossipingServicer):
     def Dessiminate(self, request, context):
         print("Dessiminate", request.text)
         self.shared.addWord(request.text)
-        context.add_callback(lambda: self.run(request.text))
+        #context.add_callback(lambda: self.run(request.text))
+        # Schedule the run method to be run asynchronously
+        t = threading.Thread(target=self.run, args=(request.text,))
+        t.start()
         return Void()
 
     def run(self, word):
@@ -43,7 +49,7 @@ class Gossiping(gossiping_pb2_grpc.GossipingServicer):
         peer = self.shared.getrand()
         print("choose", peer)
         with grpc.insecure_channel(peer) as channel:
-            stub = gossiping_pb2_grpc.GossipingStub(channel)
+            stub = rpc.GossipingStub(channel)
             response = stub.Dessiminate(Data(text=word))
         print("Sent", peer, word)
 
@@ -76,18 +82,54 @@ class Share(object):
 
 
 def newpeer(uri):
-    ip = input("ip: ")
+    with open("machines.txt") as f:
+        machines = json.load(f)
+    print("machines: ", list(machines.keys()))
+    ip = input('peer ip: ')
+    if ip in machines:
+        ip = machines[ip]
+    else:
+        print("nao encontrado")
+        sys.exit(1)
     with grpc.insecure_channel(uri) as channel:
-        stub = gossiping_pb2_grpc.GossipingStub(channel)
+        stub = rpc.GossipingStub(channel)
         response = stub.LocalRegister(Reg(ip=ip))
 
 
+def appendwords(uri):
+    lam = 5
+    while True:
+        wait = poisson(lam)
+        sleep(wait)
+        word = fetch()
+        with grpc.insecure_channel(uri) as channel:
+            stub = rpc.GossipingStub(channel)
+            response = stub.Dessiminate(Data(text=word))
+
+def fetch():
+    with open("words.txt") as f:
+        size = int(f.readline())
+        rnd = randint(1, size)
+        for _ in range(rnd):
+            f.readline()
+        word = f.readline().strip()
+        return word
+
+
 def serve():
-    myuri = input('port(50051):')
+    with open("machines.txt") as f:
+        machines = json.load(f)
+    print("machines: ", list(machines.keys()))
+    myuri = input('meu ip: ')
+    if myuri in machines:
+        myuri = machines[myuri]
+    else:
+        print("nao encontrado")
+        sys.exit(1)
     share = Share(myuri)
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
-    gossiping_pb2_grpc.add_GossipingServicer_to_server(Gossiping(share), server)
+    rpc.add_GossipingServicer_to_server(Gossiping(share), server)
     server.add_insecure_port(myuri)
     server.start()
     print("Server started, listening on " + myuri)
@@ -95,6 +137,8 @@ def serve():
     for _ in range(int(input("machines quantity: "))):
         newpeer(myuri)
 
+    t = threading.Thread(target=appendwords, args=(myuri,))
+    t.start()
     server.wait_for_termination()
 
 
